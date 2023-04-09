4f7e42b0c271971dcbd49ba3ff12a80003b25958
Anirudh282 <anirudh282sharma@gmail.com>
2023-04-09 13:19:53 +0530

    first commit

---
+++ C/client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Nav from "./components/nav"
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Routes>
+        <Route path="/account/login" element={<Login/>}/>
+        <Route path="/account/signup" element={<Signup/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
